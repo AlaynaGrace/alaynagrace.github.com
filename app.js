2b2c2eb0f3e4038920317b11d6ce7e795f26cf08
Alayna Buysse <alaynagrace@comcast.net>
2017-05-21 13:44:02 -0500

    styling looks better and about me is done

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+//requires
 var express = require('express');
 var path = require('path');
 var app = express();
 
+//makes the public folder public
 app.use(express.static('public'));
 
+//when at '/' it shows the index.html on the dom
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
 
-app.listen(3004, function() {
+//global port var so that it can be hosted on internet
+var port = process.env.PORT || 3004;
+
+//listening to port 
+app.listen(port, function() {
   console.log('up 3004');
 });
