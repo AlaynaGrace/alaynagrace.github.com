3bf53542746f3a9b9c694d2a36b190ba27d73604
Alayna Buysse <alaynagrace@comcast.net>
2017-05-21 13:48:47 -0500

    fixed refresh error

--- app.js
+++ app.js
@@ -11,10 +11,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
 
+//adding this fixed it so that I can refresh on the /code page without it saying "cannot get /code"
+app.get('/code', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
 //global port var so that it can be hosted on internet
 var port = process.env.PORT || 3004;
 
-//listening to port 
+//listening to port
 app.listen(port, function() {
   console.log('up 3004');
 });
